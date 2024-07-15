a645830c6e9104bcf50a383a45f2ea22db9f7f27
maryamamin12 <ma2084334@gmail.com>
2024-07-15 12:50:45 +0500

    string

--- class_1(string)/class_1.py
+++ class_1(string)/class_1.py
@@ -1,1 +1,42 @@
 print("hello world")
+
+print("pakistan")
+print("zinda bad")
+print("zinda bad")
+print("zinda bad")
+
+print("zinda bad abc")
+print("zinda bad 7")
+print("zinda bad 900")
+
+# This is the old way of python
+name = "Mryam Amin"
+print(name)
+print(type(name))
+print(id(name))  # physical address
+print(dir(name)) # In python the 'dir()' is used to get a list of the attributes and methods of an object.
+print([i for i in dir(name) if "_" not in i])
+
+name:str = "Maryam Amin"
+print(name)
+
+name:str = 700
+print(name)
+
+name:int = 45
+print(name)
+
+name:float = 437.6
+print(name)
+
+name = True
+print(name)
+print(type(name))
+
+a: list[str] = ['a','b','c']
+print(a)
+print(type(a))
+
+var: tuple[str,int,float] = ("pakistan",45,78.90)
+print(var)
+print(type(var))
