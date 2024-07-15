944784c0768d638bbeca20c93701bf43af1b927a
maryamamin12 <ma2084334@gmail.com>
2024-07-15 17:51:38 +0500

    f-string

--- class_2(f-string)/class_2.py
+++ class_2(f-string)/class_2.py
@@ -64,6 +64,7 @@ print(card)
 # Dir Functions.
 
 [i for i in dir(str) if "__" not in i]
+
 name : str = "maRYaM"
 print(name.capitalize())
 print(name.lower())
@@ -74,3 +75,65 @@ b = 8
 # {} place holder
 #                    0          1         0 1      
 "pakistan value a = {} and b = {}".format(a,b)
+
+
+name : str = "Maryam"
+fname : str = "Amin"
+education : str = "Matric in science"
+age : str = 18
+
+card : str = """
+PIAIC Student Card
+student Name: {1}
+Father's Name: {0}
+Age: {3}
+Education: {2}
+""".format(fname, name, education, age)
+#           0      1      2          3
+print(card)
+
+
+student_code : str = """
+print("My name is Maryam")
+a:int = 7
+b:int = 8
+print(a+b)
+"""
+
+exec(student_code)
+
+a: list[str] = [i for i in dir(str) if "__" not in i]
+print(a)
+print(len(a))
+
+name: str = "Maryam"
+# variable-name.method()
+print(name)
+print(name.casefold())
+
+# Lstrip Function
+
+name: str = "         Maryam     "
+print(name)
+print(name.lstrip()) # lstrip remove the extra spaces the left sides.
+
+
+name: str = "         Maryam     "
+print(name)
+print(name.rstrip()) # rstrip remove the extra spaces the right sides.
+
+name: str = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+
+# \n
+# \t
+# \b
+
+print("Name:\t\t Maryam Amin")
+print("Name:\n Maryam Amin")
+print("Name:\b Maryam Amin")
+
+
+nostarch_urlist:str = 'https://nostarch.com'
+nostarch_urlist.removeprefix('https://')       # prefix function remove beginner
