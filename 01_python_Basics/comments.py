4126588e22085225fbcb875686dc5a2f412e0e6d
maryamamin12 <ma2084334@gmail.com>
2026-07-24 11:04:52 +0500

    Create comments.py

---
+++ 01_python_Basics/comments.py
@@ -0,0 +1,10 @@
+# This is comment
+
+#\n that is called escape sequence character.it is use to print the next statement in the next line .
+print("I am a good girl \nand this viewer is also a good girl.")
+
+#This is a 'single-line comment'
+print("This is a print statement.")
+
+print("Hi",5 , 5, sep="-", end="009\n") #This parameters in python are optional parameters. sep is used seperate the value 
+print("maryam")
