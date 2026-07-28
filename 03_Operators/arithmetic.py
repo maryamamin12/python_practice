fc9d87e859506de09e9faedf20a1c95e71dfdd21
maryamamin12 <ma2084334@gmail.com>
2026-07-28 20:01:16 +0500

    Update arithmetic.py

---
+++ 03_Operators/arithmetic.py
@@ -0,0 +1,71 @@
+#Arithmetic operators are used to perform mathmetical operations.
+
+#Addition 
+a = 10
+b = 5
+print(a + b)
+
+num1 = 15
+num2 = 20
+result = num1 + num2
+print("Addition:", result)
+
+#Subtraction
+a = 67
+b = 8
+print(a - b)
+
+num1 = 23
+num2 = 12
+result = num1 - num2
+print("subtraction:", result)
+
+#Multiplication
+a = 15
+b = 5
+print(a * b)
+
+num1 = 12
+num2 = 2
+result = num1 * num2
+print("Multiplication:", result)
+
+#Division
+a = 20
+b = 4
+print(a / b)
+
+num1 = 70
+num2 = 7
+result = num1 / num2
+print("Division:", result)
+
+#Floor division
+a = 5
+b = 6
+print(a // b)
+
+num1 = 12
+num2 = 4
+result = num1 // num2
+print("Floor division:", result)
+
+#Modulus
+a = 12
+b = 5
+print(a % b)
+
+num1 = 35
+num2 = 5
+result = num1 % num2
+print("Modulus:", result)
+
+#Exponentiation
+a = 2
+b = 4
+print(a ** b)
+
+num1 = 4
+num2 = 6
+result = num1 ** num2
+print("Exponentiation:", result)
