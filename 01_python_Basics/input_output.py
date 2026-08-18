09db65b309417fa3d4046b16c88096563138b534
maryamamin12 <ma2084334@gmail.com>
2026-08-18 20:32:15 +0500

    Update input_output.py

--- 01_python_Basics/input_output.py
+++ 01_python_Basics/input_output.py
@@ -5,3 +5,10 @@ print(type(name))
 age = input("Enter your age:")
 print(age)
 print(type(age))
+
+x = input("Enter first number: ")
+y = input("Enter second number:")
+print("sum of two numbers is:", int(x) + int(y))
+
+height = float(input("Enter your height:"))
+print("your height is:", height)
