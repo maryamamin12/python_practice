b5c4b2e23d39429adaf8e50c520b97a2af3d9534
maryamamin12 <ma2084334@gmail.com>
2026-08-20 21:28:51 +0500

    Create string_slicing.py

---
+++ 02_Data_types/string_slicing.py
@@ -0,0 +1,21 @@
+#string slicing means taking a specific part (substring) of a string by using its indexing.
+name = "Maryam"
+print(name[0:6])
+
+language = "python"
+print(language[0:4:2])
+print(language[1:3])
+print(len(language))
+
+word = "ABCDEF"
+print(word[3:5])
+
+country = "pakistan"
+print(country[0:4])
+print(country[0:5:2])
+print(country[-3:])
+print(country[-5:-1])
+
+text = "I love python"
+print(text[7:13])
+print(len(text))
